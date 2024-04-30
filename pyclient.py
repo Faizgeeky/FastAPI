0068d167b24cd8cb0333da7981f0a5dea85397d3
Faizgeeky <nandoliyafaiz429@gmail.com>
2024-04-30 06:42:39 +0530

    feat : handle data and convert using model_to_dict

--- pyclient.py
+++ pyclient.py
@@ -4,4 +4,9 @@ endpoint = "http://127.0.0.1:8000/api"
 get_response = requests.get(endpoint, params={'data':1234}, json={"query":"select from"})
 
 # check basic home api
-print("result ", get_response.json()['message'])
+print("result ", get_response.json())
+
+
+endpoint = "http://127.0.0.1:8000/api/add-product"
+get_response = requests.get(endpoint, params={'title':"New Product 1",'content':'Very new product in trend','price':14.44}, json={"query":"select from"})
+print("result ", get_response.json())
--- restAPI/products/views.py
+++ restAPI/products/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from products.models import Product
+import json
+from django.forms.models import model_to_dict
 # Create your views here.
 
 
 def home(request):
-    return JsonResponse({"message":"from Product home"})
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    # Method 1 manual
+    # if model_data:
+    #     data['id'] = model_data.id
+    #     data['title'] = model_data.title
+    #     data['content'] = model_data.content
+    #     data['price'] = model_data.price
+    # print("Model data is ", model_data.title)
+
+    # Method model_to_dict
+    if model_data:
+        data = model_to_dict(model_data, fields=['id','title', 'price'])
+    return JsonResponse(data)
+
+
+def addProduct(request):
+    data = request.GET
+    message = ''
+    try:
+        if data['price'] and data['content'] and data['title']:
+            model_data = Product.objects.create(title=data['title'],content=data['content'],price=data[price])
+            model_data.save()
+            message = 'Data added'
+        else:
+            message = "No data is  added"
+    except:
+        pass
+    print("data is ", data)
+    return JsonResponse({'message':message})
