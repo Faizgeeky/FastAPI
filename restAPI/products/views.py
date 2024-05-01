4bf486d30f9a7f862cf78be38626f50ada514088
Faizgeeky <nandoliyafaiz429@gmail.com>
2024-05-01 07:54:18 +0530

    feat : DRF Generic Views add, view ,retrive using generivviews

--- restAPI/products/views.py
+++ restAPI/products/views.py
@@ -6,47 +6,56 @@ from django.forms.models import model_to_dict
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from products.serializers import ProductSerializer
+from rest_framework import generics
 
-@api_view(['GET','POST'])
-def home(request):
-    '''
-    DRF API VIEW
-    '''
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    # Method 1 manual
-    # if model_data:
-    #     data['id'] = model_data.id
-    #     data['title'] = model_data.title
-    #     data['content'] = model_data.content
-    #     data['price'] = model_data.price
-    # print("Model data is ", model_data.title)
 
-    # Method 3 model_to_dict
-    # if model_data:
-    #     data = model_to_dict(model_data, fields=['id','title', 'price', 'sale_price'])
+
+# @api_view(['GET','POST'])
+# def home(request):
+#     '''
+#     DRF API VIEW
+#     '''
+
+#     # data = 
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     # Method 1 manual
+#     # if model_data:
+#     #     data['id'] = model_data.id
+#     #     data['title'] = model_data.title
+#     #     data['content'] = model_data.content
+#     #     data['price'] = model_data.price
+#     # print("Model data is ", model_data.title)
+
+#     # Method 3 model_to_dict
+#     # if model_data:
+#     #     data = model_to_dict(model_data, fields=['id','title', 'price', 'sale_price'])
     
-    # Method 3 Serializers
-    instance = model_data
+#     # Method 3 Serializers
+#     instance = model_data
 
-    if instance:
-        data =  ProductSerializer(instance).data
-
-
-    return Response(data)
+#     if instance:
+#         data =  ProductSerializer(instance).data
 
 
-def addProduct(request):
-    data = request.GET
-    message = ''
-    try:
-        if data['price'] and data['content'] and data['title']:
-            model_data = Product.objects.create(title=data['title'],content=data['content'],price=data[price])
-            model_data.save()
-            message = 'Data added'
-        else:
-            message = "No data is  added"
-    except:
-        pass
-    print("data is ", data)
-    return Response({'message':message})
+#     return Response(data)
+
+# #  Validation using Serializer
+# @api_view(['POST'])
+# def addProduct(request):
+#     serializer = ProductSerializer(data = request.data)
+#     # Use raise expceptions to handle invalid or error in robust way
+#     if serializer.is_valid(raise_exception=True):
+#         instance = serializer.save()
+#         print("Instance is here", instance)
+#         return Response(serializer.data)
+
+
+
+# Generic API View
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    print("view test 1")
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
